0d9854815c55aace221f3c0d48baea2d0f05c97e
leruisi <yuristovskaya7@gmail.com>
2023-10-24 18:46:42 +0300

    DONE

--- src/components/CommentsForm/CommentsForm.js
+++ src/components/CommentsForm/CommentsForm.js
@@ -1,19 +1,27 @@
 
 import {useForm} from "react-hook-form";
 import {commentsService} from "../../services/CommentsService";
+// import {joiResolver} from "@hookform/resolvers/joi";
+// import {commentsValidator} from "../../validators/validators";
 
 const CommentsForm = ({setComments}) => {
-    const {reset,handleSubmit,register,formState:{isSubmitSuccessful}}=useForm()
+    const {reset,handleSubmit,register}=useForm()
+
+
+    // const CommentsForm = ({setComments}) => {
+    // const {reset,handleSubmit,register,formState:{isValid}}=useForm({
+    //     mode:'all',
+    //     resolver: joiResolver(commentsValidator)
+    // })
 
     const save = async (comment)=>{
      const {data}=  await commentsService.create(comment);
      setComments(prev=>[...prev,data]);
+     reset()
 
     }
 
-    if (isSubmitSuccessful){
-        reset()
-    }
+
 
 
 
@@ -23,7 +31,8 @@ const CommentsForm = ({setComments}) => {
             <input type='text' placeholder={'name'} {...register('name')}/>
             <input type='text' placeholder={'email'} {...register('email')}/>
             <input type='text' placeholder={'body'} {...register('body')}/>
-            <button >save</button>
+            {/*<button disabled={!isValid}>save</button>*/}
+            <button>save</button>
 
         </form>
     );
---
+++ src/validators/validators.js
@@ -0,0 +1,21 @@
+// import Joi from "joi";
+//
+//
+// const commentsValidator = Joi.object({
+//     name: Joi.string().regex(/^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/).required().messages({
+//             'string.pattern.base': 'lll',
+//             'string.required': ''
+//     }),
+//     // email:Joi.regex(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/).required().messages({
+//     //
+//     // }),
+//     body:Joi.string().regex(/^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/).required().messages({
+//         'string.pattern.base': 'yhh',
+//         'string.required': 'hhh'
+//     }),
+// })
+//
+// export {
+//  commentsValidator
+// }
+
