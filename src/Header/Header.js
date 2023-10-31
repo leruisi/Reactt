d4fc4c06c4440eb03afed082df5aa293c131f9c7
leruisi <yuristovskaya7@gmail.com>
2023-10-31 09:45:24 +0200

    DONE

---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import style from "./Header.module.css"
+
+const Header = () => {
+    return (
+        <div className={style.Header}>
+            Header
+        </div>
+    );
+};
+
+export {Header};
